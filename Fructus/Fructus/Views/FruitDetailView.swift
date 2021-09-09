3696c57c55e4db26c24a01982a548e1f272d524b
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-08 21:06:39 -0600

    Add nutrients section

--- Fructus/Fructus/Views/FruitDetailView.swift
+++ Fructus/Fructus/Views/FruitDetailView.swift
@@ -29,7 +29,7 @@ struct FruitDetailView: View {
                             .multilineTextAlignment(.leading)
                         
                         //: Nutrients
-                        
+                        FruitNutrientsView(fruit: fruit)
                         //: Subheadline
                         
                         Text("Learn more about \(fruit.title)".uppercased())
